648c132381de7846e2c3529db93273b120ead258
myf <myf@myfs-MacBook-Pro.local>
2024-11-08 15:49:21 +0530

    react-mention

---
+++ src/examples/CustomSuggestionsContainer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { provideExampleValue } from './higher-order'
+import defaultStyle from './defaultStyle'
+import defaultMentionStyle from './defaultMentionStyle'
+import { Mention, MentionsInput } from 'react-mentions'
+
+function CustomSuggestionsContainer({ value, data, onChange, onAdd }) {
+  return (
+    <div className="custom-suggestions">
+      <h3>Custom Suggestions Container</h3>
+
+      <MentionsInput
+        value={value}
+        onChange={onChange}
+        style={defaultStyle}
+        placeholder={"Mention people using '@'"}
+        a11ySuggestionsListLabel={"Suggested mentions"}
+        allowSuggestionsAboveCursor={true}
+        customSuggestionsContainer={(children)=><div><span style={{fontWeight: "bold"}}><h2>This container has customised suggestions</h2></span>{children}</div>}
+      >
+        <Mention data={data} onAdd={onAdd} style={defaultMentionStyle} />
+      </MentionsInput>
+    </div>
+  )
+}
+
+const asExample = provideExampleValue('')
+
+export default asExample(CustomSuggestionsContainer)
